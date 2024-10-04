b05897b173532311bac3c31f86eefd11ec74c455
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-10-04 09:35:48 +0530

    Create InsertIntoBST.java

---
+++ Trees/BST/InsertIntoBST.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            root = new TreeNode(val);
+            return root;
+        }
+
+        if(root.val > val){
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
