e135df36beb90c6fa6512685af250f45d8e30835
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-10-07 18:04:03 +0530

    Update graph.java

--- Graphs/BFS-DFS/graph.java
+++ Graphs/BFS-DFS/graph.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        List<List<Integer>> list = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        for(int[] r : edges){
+            int i = r[0];
+            int j = r[1];
+            list.get(i).add(j);
+            list.get(j).add(i);
+        }
+        
+        return list;
+    }
+}
