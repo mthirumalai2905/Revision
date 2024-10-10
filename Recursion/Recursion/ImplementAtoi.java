ef649258514c5875662c071b65d6b2b7b36baf2c
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-10-10 19:16:21 +0530

    Create ImplementAtoi.java

---
+++ Recursion/Recursion/ImplementAtoi.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+        int result = 0;
+        int sign = 1;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '+') ? 1 : -1;
+            i++;
+        }
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return sign * result;
+    }
+}
