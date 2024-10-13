aab436ea82dd05509ae5175188bf8f2593b4543e
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-10-13 10:14:39 +0530

    Create DetectCycle.java

---
+++ Graphs/BFS-DFS/DetectCycle.java
@@ -0,0 +1,31 @@
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited = new boolean[V];
+        
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                if(dfs(i, -1, visited, adj)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    public boolean dfs(int node, int parent, boolean[] visited, ArrayList<ArrayList<Integer>> adj){
+        visited[node] = true;
+        
+        for(int ngbr : adj.get(node)){
+            if(!visited[ngbr]){
+                if(dfs(ngbr, node, visited, adj)){
+                    return true;
+                }
+            } else if(ngbr != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+}
