ea2eb731c3422fe7d6aebc8a2fd2d54fd7279faa
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-11-19 22:51:18 +0530

    Update ImplementationOfSegmentTree.java

--- Trees/SegmentTree/ImplementationOfSegmentTree.java
+++ Trees/SegmentTree/ImplementationOfSegmentTree.java
@@ -1,1 +1,71 @@
 
+class SegmentTree {
+    private int[] tree;
+    private int n;
+
+    public SegmentTree(int[] arr) {
+        n = arr.length;
+        tree = new int[4 * n];
+        build(arr, 0, 0, n - 1);
+    }
+
+    private void build(int[] arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            int leftChild = 2 * node + 1;
+            int rightChild = 2 * node + 2;
+            build(arr, leftChild, start, mid);
+            build(arr, rightChild, mid + 1, end);
+            tree[node] = tree[leftChild] + tree[rightChild];
+        }
+    }
+
+    public int query(int L, int R) {
+        return query(0, 0, n - 1, L, R);
+    }
+
+    private int query(int node, int start, int end, int L, int R) {
+        if (R < start || end < L) {
+            return 0;
+        }
+        if (L <= start && end <= R) {
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        int leftChild = 2 * node + 1;
+        int rightChild = 2 * node + 2;
+        int leftSum = query(leftChild, start, mid, L, R);
+        int rightSum = query(rightChild, mid + 1, end, L, R);
+        return leftSum + rightSum;
+    }
+
+    public void update(int idx, int value) {
+        update(0, 0, n - 1, idx, value);
+    }
+
+    private void update(int node, int start, int end, int idx, int value) {
+        if (start == end) {
+            tree[node] = value;
+        } else {
+            int mid = (start + end) / 2;
+            int leftChild = 2 * node + 1;
+            int rightChild = 2 * node + 2;
+            if (start <= idx && idx <= mid) {
+                update(leftChild, start, mid, idx, value);
+            } else {
+                update(rightChild, mid + 1, end, idx, value);
+            }
+            tree[node] = tree[leftChild] + tree[rightChild];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        SegmentTree segmentTree = new SegmentTree(arr);
+        System.out.println("Sum of range [1, 3]: " + segmentTree.query(1, 3));
+        segmentTree.update(1, 10);
+        System.out.println("Sum of range [1, 3] after update: " + segmentTree.query(1, 3));
+    }
+}
