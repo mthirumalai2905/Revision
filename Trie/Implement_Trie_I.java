0dda0a2de63a2b63566ecd6c2bd7d91f47aad49d
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-11-23 22:42:23 +0530

    Create Implement_Trie_I.java

---
+++ Trie/Implement_Trie_I.java
@@ -0,0 +1,45 @@
+class Trie {
+    class TrieNode {
+        boolean isEndOfWord = false;
+        TrieNode[] children = new TrieNode[26];
+    }
+
+    public TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                node.children[ch - 'a'] = new TrieNode();
+            }
+            node = node.children[ch - 'a'];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+        return node.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+        return true;
+    }
+}
