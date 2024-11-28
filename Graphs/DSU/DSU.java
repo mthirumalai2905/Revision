366bd1b3684308e401545e5d81b9c107fc60e4df
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-11-28 14:30:24 +0530

    Update DSU.java

--- Graphs/DSU/DSU.java
+++ Graphs/DSU/DSU.java
@@ -1,1 +1,25 @@
+class DSU{
+  private int[] parent;
 
+  public DSU(int n){
+    parent = new int[n];
+    for(int i = 0; i < n; i++){
+      parent[i] = i;
+    }
+  }
+
+ public int find(int x){
+   if(x == parent[x]){
+     return x;
+   }
+   return find(parent[x]);
+ }
+
+public void union(int x, int y){
+  int rootX = find(x);
+  int rootY = find(y);
+  if(rootX != rootY){
+    parent[rootX] = rootY;
+  }
+ }
+}
