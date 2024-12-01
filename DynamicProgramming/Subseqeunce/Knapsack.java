fb275522b71b343bc942461e5958929922d7fa88
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-12-01 19:35:02 +0530

    Update Knapsack.java

--- DynamicProgramming/Subseqeunce/Knapsack.java
+++ DynamicProgramming/Subseqeunce/Knapsack.java
@@ -1,30 +1,25 @@
-
-class Solution {
-    static int knapSack(int W, int wt[], int val[]) {
-        int n = wt.length;
-        int[][] memo = new int[n+1][W+1];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-        return solve(W, n, wt, val, memo, 0);
+class Solution{
+    public int knapsack(int[] weights, int[] values, int n, int W){
+        Integer[][] dp = new Integer[n][W+1];
+        return solve(dp,weights,values,W,0);
     }
-
-    static int solve(int W, int n, int[] wt, int[] val, int[][] memo, int idx) {
-        if (idx >= n || W == 0) {
-            return 0;
+    public static int solve(Integer[][] dp, int[] weights, int[] values, int remainingWeight, int idx){
+        if(idx >= weights.length || remainingWeight == 0){
+            return  0;
         }
 
-        if (memo[idx][W] != -1) {
-            return memo[idx][W];
+        if(dp[idx][remainingWeight] != null){
+            return dp[idx][remainingWeight];
         }
 
-        if (wt[idx] > W) {
-            return memo[idx][W] = solve(W, n, wt, val, memo, idx + 1);
+        int take = 0;
+        if(weights[idx] <= remainingWeight){
+            take = values[idx] + solve(dp, weights, values, remainingWeight - weights[idx], idx + 1)
         }
 
-        int take = val[idx] + solve(W - wt[idx], n, wt, val, memo, idx + 1);
-        int notTake = solve(W, n, wt, val, memo, idx + 1);
+        int notTake = solve(dp, weights, values, remainingWeight, idx + 1);
 
-        return memo[idx][W] = Math.max(take, notTake);
+        return dp[idx][remaingingWeight] = Math.max(take, notTake);
     }
 }
+        
