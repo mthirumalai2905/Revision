dfdf310352984781f902e3aa9e9ad9723ba2192c
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-12-03 11:24:00 +0530

    Update KMP.java

--- Strings/KMP.java
+++ Strings/KMP.java
@@ -1,63 +1,44 @@
 public class KMPAlgorithm {
-
-    // Function to preprocess the pattern and build the LPS array
-    public static int[] computeLPSArray(String pattern) {
-        int m = pattern.length();
-        int[] LPS = new int[m];
-        int length = 0;  // Length of the previous longest prefix suffix
-        int i = 1;
-
-        // Build the LPS array
-        while (i < m) {
-            if (pattern.charAt(i) == pattern.charAt(length)) {
-                length++;
-                LPS[i] = length;
-                i++;
+    public static int[] computeLPS(String pattern) {
+        int n = pattern.length();
+        int[] lps = new int[n];
+        int len = 0, i = 1;
+        while (i < n) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                lps[i++] = ++len;
+            } else if (len > 0) {
+                len = lps[len - 1];
             } else {
-                if (length != 0) {
-                    length = LPS[length - 1];  // Use the previous LPS value
-                } else {
-                    LPS[i] = 0;
-                    i++;
-                }
+                lps[i++] = 0;
             }
         }
-        return LPS;
+        return lps;
     }
 
-    // Function to perform KMP search
-    public static void KMPSearch(String text, String pattern) {
-        int n = text.length();
-        int m = pattern.length();
-        int[] LPS = computeLPSArray(pattern);
-
-        int i = 0;  // Index for text
-        int j = 0;  // Index for pattern
-
-        while (i < n) {
+    public static boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPS(pattern);
+        int i = 0, j = 0;
+        while (i < text.length()) {
             if (pattern.charAt(j) == text.charAt(i)) {
                 i++;
                 j++;
             }
-
-            if (j == m) {
-                System.out.println("Pattern found at index " + (i - j));
-                j = LPS[j - 1];
-            } else if (i < n && pattern.charAt(j) != text.charAt(i)) {
-                // Mismatch occurs
-                if (j != 0) {
-                    j = LPS[j - 1];  // Skip comparisons based on LPS array
+            if (j == pattern.length()) {
+                return true;
+            } else if (i < text.length() && pattern.charAt(j) != text.charAt(i)) {
+                if (j > 0) {
+                    j = lps[j - 1];
                 } else {
                     i++;
                 }
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
-        String text = "ABABDABACDABABCABAB";
-        String pattern = "ABABCABAB";
-        
-        KMPSearch(text, pattern);
+        String text = "ababcabcabababd";
+        String pattern = "ababd";
+        System.out.println(kmpSearch(text, pattern));
     }
 }
