50495020c6b8966be3c72ee0ff8279711c0140b5
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-12-21 13:28:28 +0530

    Create MooreVoting.java

---
+++ NamedAlgo/MooreVoting.java
@@ -0,0 +1,12 @@
+public class MooreVoting {
+    public int majorityElement(int[] nums) {
+        int candidate = -1, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
