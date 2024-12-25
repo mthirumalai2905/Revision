029f8ab44dd9f0e8e86e4d237dc27ef90a685286
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-12-25 22:36:57 +0530

    Update FloydWarshal.java

--- Graphs/ShortestPath/FloydWarshal.java
+++ Graphs/ShortestPath/FloydWarshal.java
@@ -1,1 +1,53 @@
+class Main {
+    public static int[][] floydWarshall(int[][] graph) {
+        int n = graph.length;
+        int[][] dist = new int[n][n];
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0; 
+                } else if (graph[i][j] != 0) {
+                    dist[i][j] = graph[i][j]; 
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE; 
+                }
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 3, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 7},
+            {0, 0, 2, 0}
+        };
+
+        int[][] distances = floydWarshall(graph);
+
+        System.out.println("Shortest distances between every pair of vertices:");
+        for (int i = 0; i < distances.length; i++) {
+            for (int j = 0; j < distances[i].length; j++) {
+                if (distances[i][j] == Integer.MAX_VALUE) {
+                    System.out.print("INF ");
+                } else {
+                    System.out.print(distances[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
