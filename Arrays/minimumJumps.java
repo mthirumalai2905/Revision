00e178c8595db3329ad2994d336a4759abddbaf3
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2025-01-22 10:36:53 +0530

    Create minimumJumps.java

---
+++ Arrays/minimumJumps.java
@@ -0,0 +1,34 @@
+class Solution {
+    static int minJumps(int[] arr) {
+        // code here
+        int n = arr.length;
+        
+        if(arr[0] == 0){
+            return -1;
+        }
+        
+        if(arr.length <= 1){
+            return 0;
+        }
+        
+        int maxR = 0;
+        int maxS = 0;
+        int j = 0;
+        
+        for(int i = 0; i < n; i++){
+            maxR = Math.max(maxR, i + arr[i]);
+            
+            if(i == maxS){
+                j++;
+                maxS = maxR;
+                
+                
+                if(maxS >= n-1){
+                    return j;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
