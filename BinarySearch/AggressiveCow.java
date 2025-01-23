90688bc6eb93cf56d9392da661e3af5065bae45c
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2025-01-23 10:21:13 +0530

    Update AggressiveCow.java

--- BinarySearch/AggressiveCow.java
+++ BinarySearch/AggressiveCow.java
@@ -1,33 +1,41 @@
-// User function Template for Java
+import java.util.Arrays;
+
 class Solution {
-    public static int aggressiveCows(int[] stalls, int k) {
+    public int aggressiveCows(int[] stalls, int n) {
         Arrays.sort(stalls);
-        int left = 1, right = stalls[stalls.length - 1] - stalls[0];
+        
+        int left = 1; 
+        int right = stalls[stalls.length - 1] - stalls[0]; 
         int result = 0;
-
+        
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (canPlace(stalls, k, mid)) {
-                result = mid;
-                left = mid + 1;
+            int mid = left + (right - left) / 2; 
+            
+            if (canPlaceCows(stalls, n, mid)) {
+                result = mid; 
+                left = mid + 1; 
             } else {
                 right = mid - 1;
             }
         }
+        
         return result;
     }
-
-    private static boolean canPlace(int[] stalls, int k, int dist) {
-        int count = 1, lastPos = stalls[0];
-
+    
+    private boolean canPlaceCows(int[] stalls, int n, int distance) {
+        int count = 1; 
+        int lastPlaced = stalls[0];
+        
         for (int i = 1; i < stalls.length; i++) {
-            if (stalls[i] - lastPos >= dist) {
-                count++;
-                lastPos = stalls[i];
+            if (stalls[i] - lastPlaced >= distance) {
+                count++; 
+                lastPlaced = stalls[i];
             }
-            if (count == k) return true;
+            if (count == n) {
+                return true; 
+            }
         }
-        return false;
+        
+        return false; // Not enough stalls for this distance
     }
 }
